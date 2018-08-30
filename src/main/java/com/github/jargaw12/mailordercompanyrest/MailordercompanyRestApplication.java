893281d820a5a0a938175d95e9ce2cb285cad970
jargaw12 <33065466+jargaw12@users.noreply.github.com>
2018-08-30 20:03:10 +0200

    Initial commit

---
+++ src/main/java/com/github/jargaw12/mailordercompanyrest/MailordercompanyRestApplication.java
@@ -0,0 +1,12 @@
+package com.github.jargaw12.mailordercompanyrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailordercompanyRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailordercompanyRestApplication.class, args);
+    }
+}
